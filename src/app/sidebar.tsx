bebe5ea316ec109546e1eb129f9c9d819cc41933
Ceglédi Zente Holló <czh@czh.hu>
2023-11-10 19:58:22 +0100

    Sidebar

--- src/app/sidebar.tsx
+++ src/app/sidebar.tsx
@@ -1,10 +1,9 @@
 import Image from 'next/image'
 import {Users02, Users01, ImageIndentLeft, PieChart01, User02, UserPlus02, UsersPlus, User01, Flag01, Flag02, Flag03, Flag04, Flag06, Flag05, Hourglass01, Hourglass02, Hourglass03, UserPlus01, File01, PlusCircle, File02, File03, File04, FilePlus01, FilePlus03, GitBranch01, LogOut01, LogOut02, Edit01, Edit02, Edit03} from "@untitled-ui/icons-react";
 import { File } from 'buffer';
-import Sidebar from './sidebar.tsx';
 
 
-export default function Home() {
+export default function Sidebar() {
   return (
     <>
       <div className="drawer lg:drawer-open">
