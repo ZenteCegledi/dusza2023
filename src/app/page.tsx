a7779912e1f6e489bce90919deb58e49b9c92ddf
Ceglédi Zente Holló <czh@czh.hu>
2023-11-10 18:49:50 +0100

    Change icon library

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,33 @@
 import Image from 'next/image'
+import {UsersPlus} from "@untitled-ui/icons-react";
 
 
 export default function Home() {
   return (
     <>
-  <div className="drawer lg:drawer-open">
-  <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
-  <div className="drawer-content flex flex-col items-center justify-center">
-    {/* Page content here */}
-    <label htmlFor="my-drawer-2" className="btn btn-primary drawer-button lg:hidden">Open drawer</label>
-  
-  </div> 
-  <div className="drawer-side">
-    <label htmlFor="my-drawer-2" aria-label="close sidebar" className="drawer-overlay"></label> 
-    <ul className="menu p-4 w-80 min-h-full bg-base-200 text-base-content">
-      {/* Sidebar content here */}
-      <li><a>Sidebar Item 1</a></li>
-      <li><a>Sidebar Item 2</a></li>
-    </ul>
-  
-  </div>
-</div>
-</>
-
+      <div className="drawer lg:drawer-open">
+        <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
+        <div className="drawer-content flex flex-col items-center justify-center">
+          {/* Page content here */}
+          {/* <label htmlFor="my-drawer-2" className="btn btn-primary drawer-button lg:hidden">
+            <ActivityHeart />
+            Open drawer
+          </label> */}
+        </div> 
+        <div className="drawer-side">
+          <label htmlFor="my-drawer-2" aria-label="close sidebar" className="drawer-overlay"></label> 
+          <ul className="menu p-4 w-80 min-h-full bg-base-200 text-base-content">
+            {/* Sidebar content here*/}
+            <li><a>Dashboard</a></li>
+            <li><a>Versenyző hozzáadása</a></li>
+            <li><a>Versenyző hozzáadása</a></li>
+            <li><a>Versenyző hozzáadása</a></li>
+            <li><a>Versenyző hozzáadása</a></li>
+            <li><a>Versenyző hozzáadása</a></li>
+            <li><a><UsersPlus/> Versenyző hozzáadása</a></li>
+          </ul>
+        </div>
+      </div>
+    </>
   )
 }
