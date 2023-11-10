8467795d665327dd5fae0a151179c5ff2ab8ac62
Ceglédi Zente Holló <czh@czh.hu>
2023-11-10 23:45:22 +0100

    Feladatok

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import SidebarOpener from './sidebarOpener';
 
 
 
-export default function AdminFrame() {
+export default function Home() {
   return (
     <>
       <div className="drawer lg:drawer-open">
@@ -13,19 +13,9 @@ export default function AdminFrame() {
         
         <div className="relative flex flex-col justify-center h-screen overflow-hidden">
         <div className="w-full p-6 m-auto bg-black rounded-md shadow-md lg:max-w-lg bg-gray-900">
-            <h1 className="text-3xl font-semibold text-center text-white-700 pb-5">Felhasználó létrehozása</h1>
+            <h1 className="text-3xl font-semibold text-center text-white-700 pb-5">Csapat létrehozása</h1>
             <form className="space-y-4">
-            <hr className=" h-px my-2 bg-gray-200 border-0 dark:bg-gray-700"/>
-                <div className="pb-5">
-                    <label className="label">
-                        <span className="text-base label-text">Versenyzők feltöltése (<a href='' className='text-blue-300'>minta</a>)</span>
-                    </label>
-                    <input type="file" className="file-input file-input-bordered " />
-                </div>
-                <div className="inline-flex items-center justify-center w-full">
-                <hr className="w-64 h-px my-2 bg-gray-200 border-0 dark:bg-gray-700"/>
-                <span className="absolute px-3 font-medium text-gray-900 -translate-x-1/2 bg-white left-1/2 dark:text-white dark:bg-gray-900">vagy</span>
-              </div>
+            
               <div>
                     <label className="label">
                         <span className="text-base label-text">Név</span>
