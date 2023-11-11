81c11bd1438b672c5e3ecc99ce4a2cfbd371aa29
Ceglédi Zente Holló <czh@czh.hu>
2023-11-11 17:13:56 +0100

    Introduction page

---
+++ src/app/introduction/page.tsx
@@ -0,0 +1,12 @@
+import Link from 'next/link'
+
+export default function Introduction() {
+  return (
+    <div className='bg-base-100'>
+      <div className="drawer lg:drawer-open bg-base-100 container mx-auto p-5 px-10 mt-20 container bg-zinc-700 rounded-xl pb-12">
+        <h1 className='text-5xl pb-14 pt-8'>((Cím))</h1>
+        <div className='text-2xl'>((Oldal tartalma))</div>
+      </div>
+      </div>
+  )
+}
