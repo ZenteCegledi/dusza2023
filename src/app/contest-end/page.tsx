ca8224458dc1513b9d1741b60814a2b3e44ebca5
Ceglédi Zente Holló <czh@czh.hu>
2023-11-11 22:01:31 +0100

    Contest End Update

--- src/app/contest-end/page.tsx
+++ src/app/contest-end/page.tsx
@@ -18,8 +18,8 @@ export default async function ContestEnd() {
 
           <div className='pt-2'>
             <div className='flex items-center justify-center'>
-            <Link href="../" className='mt-3 mx-1 btn bg-yellow-950 hover:bg-yellow-900'>Bemutatkozó oldal<Home03/></Link>
-            <Link href="../introduction" className='mt-3 mx-1 btn bg-zinc-700 hover:bg-zinc-600'> Kijelentkezés<LogOut01/></Link>
+            <Link href="../introduction" className='mt-3 mx-1 btn bg-yellow-950 hover:bg-yellow-900'>Bemutatkozó oldal<Home03/></Link>
+            <Link href="../" className='mt-3 mx-1 btn bg-zinc-700 hover:bg-zinc-600'> Kijelentkezés<LogOut01/></Link>
           </div>
           </div>
       </div>
