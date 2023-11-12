d9ac9abcfeb4cfe9436f9f9ce6c22b4989850b8b
Zoli06 <zolixvagyok@gmail.com>
2023-11-12 22:06:43 +0100

    Home page buttons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import { Home02, Laptop01, LogIn02 } from '@untitled-ui/icons-react';
+import { Award05, Home02, Laptop01, LogIn02 } from '@untitled-ui/icons-react';
 import { fetchIntro } from './utils/fetchers/intro';
 import { fetchSettings } from './utils/fetchers/settings';
 import Link from 'next/link';
@@ -13,23 +13,32 @@ export default async function Home() {
           <h1 className='text-6xl font-bold pb-12 pt-10'>{settings.name}</h1>
           <p className='text-3xl'>{settings.slogan}</p>
           <div className='mt-10 mb-20'>
-          <div>
-            <Link
-              className='btn bg-blue-600 hover:bg-blue-800 pl-10 pr-10 mr-5 text-white'
-              href='/login'>
-              <LogIn02 />
-              Bejelentkezés
-            </Link>
-            <Link href="/introduction" className='btn bg-neutral-700 hover:bg-neutral-800 ml-5 pl-4 pr-4 text-white'>
-              <Home02 />
-              Bemutatkozó oldal
-            </Link>
-            </div>
-            <div className='flex flex-col justify-center'>
-            <Link href="./introduction-programmers" className='btn bg-yellow-700 hover:bg-yellow-800 mt-5 text-white'>
-              <Laptop01 />
-              Programozók bemutatkozása
-            </Link>
+            <div className='flex flex-col justify-center gap-4'>
+              <Link
+                className='btn bg-blue-600 hover:bg-blue-800 text-white'
+                href='/login'
+              >
+                <LogIn02 />
+                Bejelentkezés
+              </Link>
+              <Link
+                href='/introduction'
+                className='btn bg-neutral-700 hover:bg-neutral-800 text-white'
+              >
+                <Home02 />
+                Bemutatkozó oldal
+              </Link>
+              <Link
+                href='/introduction-programmers'
+                className='btn bg-yellow-700 hover:bg-yellow-800 text-white'
+              >
+                <Laptop01 />
+                Programozók bemutatkozása
+              </Link>
+              <Link href='/contests' className='btn bg-green-800 hover:bg-green-700'>
+                <Award05 />
+                Versenyek
+              </Link>
             </div>
           </div>
         </div>
